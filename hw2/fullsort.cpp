e06885fd52ac3dd72f0d4493186e50b4d205448d
Chengyi(Jeff) Chen <chen364@usc.edu>
2018-01-22 01:44:06 -0800

    Updated fullsort.cpp

--- hw2/fullsort.cpp
+++ hw2/fullsort.cpp
@@ -8,10 +8,47 @@ struct Node {
     Node *next;
 };
 
+Node* head = NULL;
+
+void insert(int x) {
+	Node* temp = new Node;
+	temp->value = x;
+	temp->next = head;
+	head = temp;
+}
+
+void traverse(Node* h) {
+	Node* temp = h;
+	while(temp != NULL) {
+		cout << temp->value;
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+//Node* fullsort (Node*);
+
 int main (int argc, char* argv[])
 {
+	if(argc < 2){
+    cerr << "Please provide a list of integers separated by whitespace." << endl;
+    return 1;
+	}
 
-  Node* fullsort (Node* in);
+	for(int i = 1; i < argc; i++) {
+		Node* curr = new Node;
+		curr->value = atoi(argv[i]);
+		temp->next = curr;
+	}
 
-  return 0;
+	while(head != NULL) {
+		head
+	}
+
+	//Node* fullsort (Node* in);
+	return 0;
 }
+/*
+Node* fullsort(Node* in) {
+
+}*/
