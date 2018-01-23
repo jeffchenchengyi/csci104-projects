6943718234e9a97b0effed7e14373f442a549a8e
Chengyi(Jeff) Chen <chen364@usc.edu>
2018-01-22 23:22:57 -0800

    Updated a few files

--- hw2/lliststr.cpp
+++ hw2/lliststr.cpp
@@ -19,36 +19,48 @@ LListStr::~LListStr() {
 /**
  * Returns the current number of items in the list
  */
-int LListStr::size() const;
+int LListStr::size() const {
+
+}
 
 /**
  * Returns true if the list is empty, false otherwise
  */
-bool LListStr::empty() const;
+bool LListStr::empty() const {
+
+}
 
 /**
  * Inserts val so it appears at the index given as pos. If the
  * index pos is invalid, this function should return without
  * modifying the list.
  */
-void LListStr::insert(int pos, const string &val);
+void LListStr::insert(int pos, const string &val) {
+
+}
 
 /**
  * Removes the value at the index given by pos. If the
  * index pos is invalid, this function should return without
  * modifying the list.
  */
-void LListStr::remove(int pos);
+void LListStr::remove(int pos) {
+
+}
 
 /**
  * Overwrites the old value at the index given by pos with val. If the
  * index pos is invalid, this function should return without
  * modifying the list.
  */
-void LListStr::set(int pos, const string &val);
+void LListStr::set(int pos, const string &val) {
+
+}
 
 /**
  * Returns the value at the index given by pos.  If the index pos is invalid,
  * then you should return the empty string.
  */
-LListStr::string get(int pos) const;
+LListStr::string get(int pos) const {
+	
+}
