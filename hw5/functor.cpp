124d73662e87ee28dff9863b4327983a43d41323
Chengyi(Jeff) Chen <chen364@usc.edu>
2018-03-12 19:00:03 -0700

    Finished problem 1a

--- hw5/functor.cpp
+++ hw5/functor.cpp
@@ -1,59 +1,35 @@
 #include <iostream>
 #include <stdlib.h>
-#include "stackstr.h"
-#include "lliststr.h"
+#include "functor.h"
+#include <string>
 
 using namespace std;
 
-StackStr::StackStr() {
-   /** 
-	* Single data member of the Stack.
-    * We use composition to implement this Stack 
-    */
+//Sums up all the int values of the digits according to HW5:
+//("0" = 0, "1" = 1, "2" = 2, ..., "9" = 9, "A" = 10, "B" = 11, "C" = 12, ..., "Z" = 35
+int NumStrComp::stringSum(const string& str) {
+	int sum = 0;
+	for(int i = 0; i < int(str.size()); i++) {
+   		if(isalnum(str[i])) {
+            if(isalpha(str[i])) {
+            	sum += (int(toupper(str[i])) - int('0') - 7); 
+            }
+            else if(isdigit(str[i])) {
+				sum += (int(str[i]) - int('0')); 
+			}
+        }
+   	}
+   	return sum;
 }
 
-StackStr::~StackStr() {
-}
-
-/**
- * Returns true if the stack is empty, false otherwise
- */
-bool StackStr::empty() const {
-	return list_.empty();
-}
-
-/**
- * Returns the number of elements in the stack.
- */
-int StackStr::size() const {
-	return list_.size();
-}
-
-/**
- * Pushes a new value, str, onto the top of the stack
-  */
-void StackStr::push(const string &str) {
-	list_.insert(0, str);
-}
-
-/**
- * Returns the top value on the stack.
- * If the stack is empty, return the empty string.
- */
-string StackStr::top() const {
-	if(list_.size() == 0) {
-		return "";
+bool NumStrComp::operator()(const string& lhs, const string& rhs) {
+	int lhs_sum = stringSum(lhs);
+	int rhs_sum = stringSum(rhs);
+	if(lhs_sum != rhs_sum) {
+		return lhs_sum < rhs_sum;
 	} else {
-		return list_.get(0);
-	}
-}
-
-/**
- * Removes the top element on the stack.
- * Should do nothing if the stack is empty.
- */
-void StackStr::pop() {
-	if(!list_.empty()) {
-		list_.remove(0);
+	    // Uses string's built in operator< 
+	    // to do lexicographic (alphabetical) comparison
+	    return lhs < rhs; 
 	}
 }
