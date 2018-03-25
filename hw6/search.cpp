e1550fff15def46adf58c3cd3db092c218d4958c
Chengyi(Jeff) Chen <chen364@usc.edu>
2018-03-24 21:01:18 -0700

    Finished problem 1 and 2

--- hw6/search.cpp
+++ hw6/search.cpp
@@ -259,15 +259,61 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 /*------------- START INITIALIZE REQUIRED FILES -------------*/
-    //To check if the input, query, and output file was also included as a command line parameter
-    if (argc < 4) {
-       cerr << "Please provide input, query, and output file" << endl;
-       return -1;
+    //To check if the config file was also included as a command line parameter
+    ifstream config;
+    if(argc < 2) {
+    	cout << "*Default config.txt will be used*" << endl;
+    	config.open("config.txt");
+    } else {
+    	config.open(argv[1]);
     }
 
-    ifstream input(argv[1]);
-    ifstream query(argv[2]);
-    ofstream output(argv[3]);
+    //If the config file given could not be found or could not be open
+    if(config.fail()) {
+        cerr << "Configuration file could not be open." << endl;
+        return -1;
+    }
+/*------------- END INITIALIZE REQUIRED FILES -------------*/
+
+/*------------- START CONFIGURATION VARIABLES -------------*/
+    string INDEX_FILE, QUERY_FILE, OUTPUT_FILE;
+    string config_line;
+    while(getline(config, config_line)) {
+    	string data, variable;
+    	if(!config_line.empty()) {
+    		stringstream ss(config_line);
+    		char curr_char;
+    		while(ss >> curr_char) {
+    			if(curr_char == '=') {
+	    			while(ss >> curr_char) {
+	    				if(curr_char == '#') {
+	    					break;
+	    				} else {
+	    					data.append(string(1, curr_char));
+	    				}
+	    			}
+	  				break;
+	   			} else if(curr_char == '#') {
+	   				break;
+	   			} else {
+	  				variable.append(string(1, curr_char));
+	   			}
+			}
+    	}
+    	if(!data.empty()) {
+    		if(variable == "INDEX_FILE") {
+    			INDEX_FILE = data;
+    		} else if(variable == "QUERY_FILE") {
+    			QUERY_FILE = data;
+    		} else if(variable == "OUTPUT_FILE") {
+    			OUTPUT_FILE = data;
+    		}
+    	}
+    }
+
+    ifstream input(INDEX_FILE);
+    ifstream query(QUERY_FILE);
+    ofstream output(OUTPUT_FILE);
 
     //If the input file given could not be found or could not be open
     if(input.fail()) {
@@ -286,7 +332,7 @@ int main(int argc, char* argv[])
         cout << "Output file could not be open." << endl;
         return 1;
     }
-/*------------- END INITIALIZE REQUIRED FILES -------------*/
+/*------------- END CONFIGURATION VARIABLES -------------*/
 
 /*------------- START MAIN VARIABLES -------------*/
     string weblink; //"data/webpg1.txt", "data/webpg2.txt", "data/webpg3.txt"
@@ -334,5 +380,6 @@ int main(int argc, char* argv[])
     	delete (*webpage_itr);
 	}
 /*------------- END DEALLOCATE WEBPAGE DYNAMIC MEM -------------*/
+
     return 0;
 }
